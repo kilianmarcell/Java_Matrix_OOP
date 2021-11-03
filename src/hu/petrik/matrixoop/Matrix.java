284cfc4e8f5472d7ec6b37a85adb0c49c8d7c012
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-03 14:56:46 +0100

    Mátrix osztály létrehozva, mátrix feltöltve

---
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -0,0 +1,26 @@
+package hu.petrik.matrixoop;
+
+public class Matrix {
+
+    private int sorokSzama;
+    private int oszlopokSzama;
+    private int[][] matrix;
+
+    public Matrix(int sorokSzama, int oszlopokSzama, int[][] matrix) {
+        this.sorokSzama = sorokSzama;
+        this.oszlopokSzama = oszlopokSzama;
+        this.matrix = matrix;
+    }
+
+    public Matrix() {
+        this.sorokSzama = (int)(Math.random() * 11) + 5;
+        this.oszlopokSzama = (int)(Math.random() * 11) + 5;
+        this.matrix = new int[this.sorokSzama][this.oszlopokSzama];
+
+        for (int i = 0; i < this.sorokSzama; i++) {
+            for (int j = 0; j < this.oszlopokSzama; j++) {
+                this.matrix[i][j] = (int)(Math.random() * 90) + 10;
+            }
+        }
+    }
+}
